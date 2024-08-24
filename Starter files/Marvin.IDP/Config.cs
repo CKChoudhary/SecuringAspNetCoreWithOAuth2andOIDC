697fc527b7302d81a5da3efa90cf36e642377d6b
Chanchal Choudhary <chanchal.choudhary@fcsamerica.com>
2024-08-24 08:18:36 -0500

    enabled authentication on client

--- Starter files/Marvin.IDP/Config.cs
+++ Starter files/Marvin.IDP/Config.cs
@@ -7,7 +7,7 @@ public static class Config
 {
     public static IEnumerable<IdentityResource> IdentityResources =>
         new IdentityResource[]
-        { 
+        {
             new IdentityResources.OpenId(),
             new IdentityResources.Profile()
         };
@@ -17,13 +17,18 @@ public static class Config
             { };
 
     public static IEnumerable<Client> Clients =>
-        new Client[] 
-            { new Client(){ ClientName = "Image Gallery", ClientId = "imagegalleryclient", AllowedGrantTypes = GrantTypes.Code, 
-                RedirectUris = {"https://localhost:7184/signin-oidc"}, AllowedScopes =
+        new Client[]
+            { new Client(){ 
+                ClientName = "Image Gallery", 
+                ClientId = "imagegalleryclient",
+                AllowedGrantTypes = GrantTypes.Code,
+                RedirectUris = {"https://localhost:7184/signin-oidc"}, 
+                AllowedScopes =
                 {
                     IdentityServerConstants.StandardScopes.OpenId,
                     IdentityServerConstants.StandardScopes.Profile
                 },
-                ClientSecrets = { new Secret("secret".Sha256())}
+                ClientSecrets = { new Secret("secret".Sha256())},
+                RequireConsent = true
             } };
 }
